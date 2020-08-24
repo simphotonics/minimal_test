8ba177fddcd130453ecd7b06c240543667aee29b
simphotonics <git@simphotonics.com>
2020-08-24 15:01:14 +0100

    Added docs.

--- lib/src/color_options.dart
+++ lib/src/color_options.dart
@@ -5,22 +5,25 @@ enum ColorOutput { ON, OFF }
 /// Ansi color modifier: Reset to default.
 const String RESET = '\u001B[0m';
 
-/// Ansi color modifier.
+/// Ansi color modifier: blue foreground.
 const String BLUE = '\u001B[34m';
 
-/// Ansi color modifier.
+/// Ansi color modifier: cyan foreground.
 const String CYAN = '\u001B[36m';
 
-/// Ansi color modifier.
+/// Ansi color modifier: cyan bold text.
+const String CYAN_BOLD = '\u001B[1;36m';
+
+/// Ansi color modifier: green foreground.
 const String GREEN = '\u001B[32m';
 
-/// Ansi color modifier.
+/// Ansi color modifier: red foreground.
 const String RED = '\u001B[31m';
 
-/// Ansi color modifier.
+/// Ansi color modifier: yellow foreground.
 const String YELLOW = '\u001B[33m';
 
-/// Ansi color modifier.
+/// Ansi color modifier: magenta foreground.
 const String MAGENTA = '\u001B[35m';
 
 /// Transforms error/exception messages to an output string.
