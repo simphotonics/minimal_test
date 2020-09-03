4c48ae38193fbdc7216b376673c18359d968cd8c
simphotonics <git@simphotonics.com>
2020-09-03 08:53:46 +0100

    Amended script usage.

--- bin/minimal_test.dart
+++ bin/minimal_test.dart
@@ -5,30 +5,42 @@ import 'package:minimal_test/script_dependencies.dart';
 
 /// The script usage.
 const usage = '\n'
-    'Usage: minimal_test <test-directory/test-file> '
+    'Usage: minimal_test [<test-directory/test-file>] '
     '[options]\n\n'
-    '  Note: If a test-directory is specified, the program  will attempt \n'
-    '        to run all dart files ending with \'_test.dart.\'\n\n'
+    '  Notes: - If a test-directory is specified, the program  will attempt \n'
+    '           to run all dart files ending with \'_test.dart.\'\n'
+    '         - Glob-style wilde-cards are not supported.\n\n'
     '  Options:\n'
     '    -h, --help                Shows script usage.\n'
     '    -v, --verbose             Enables displaying error messages.\n'
     '    --disable-color           Disables color output.\n';
 
 Future<void> main(List<String> args) async {
-  // Resolving test files.
-  if (args.isEmpty) {
-    print('Please specify a path to a test directory or a test file.');
-    print(usage);
-    exit(ExitCode.NoTestFilesFound.index);
-  } else if (args[0] == '-h' || args[0] == '--h') {
-    print(usage);
-    exit(ExitCode.NoTestFilesFound.index);
+  final _args = List.from(args);
+
+  // Reading script options.
+  final colorOutput =
+      _args.contains('--disable-color') ? ColorOutput.OFF : ColorOutput.ON;
+  final isVerbose = _args.contains('--verbose') || _args.contains('-v');
+  _args.remove('--disable-color');
+  _args.remove('--verbose');
+  _args.remove('-v');
+
+  if (_args.isEmpty) {
+    _args.add('test');
   }
 
-  final testFiles = await resolveTestFiles(args[0]);
+  if (_args[0] == '-h' || _args[0] == '--help') {
+    print(usage);
+    exit(0);
+  }
 
+  // Resolving test files.
+  final testFiles = await resolveTestFiles(_args[0]);
   if (testFiles.isEmpty) {
-    print('Could not resolve any test files using path: ${args[0]}');
+    print('Could not resolve any test files using path: ${_args[0]}');
+    print('Please specify a path to a test directory containing test files.');
+    print(usage);
     exit(ExitCode.NoTestFilesFound.index);
   } else {
     print('Finding test files: ');
@@ -37,69 +49,45 @@ Future<void> main(List<String> args) async {
     }
   }
 
-  // Reading script options
-  final colorOutput =
-      args.contains('--disable-color') ? ColorOutput.OFF : ColorOutput.ON;
-  final isVerbose = args.contains('--verbose');
-
-  // final results = Future<Map<String, ProcessResult>>(() {
-  //   final map = <String, ProcessResult>{};
-
-  //   for (final file in testFiles) {
-  //     Process.run(
-  //       'dart',
-  //       [
-  //         '--enable-experiment=non-nullable',
-  //         file.path,
-  //       ],
-  //     ).then((process) {
-  //       map[file.path] = process;
-  //     });
-  //   }
-
-  //   return map;
-  // });
-  final exitCodes = <String, Completer<int>>{};
-  final stderrOutput = <String, Completer<String>>{};
-  final results = <String, Future<ProcessResult>>{};
-
   // Starting processes.
+  final fResults = <Future<TestProcessResult>>[];
   for (final file in testFiles) {
-    exitCodes[file.path] = Completer<int>();
-    stderrOutput[file.path] = Completer<String>();
-
-    results[file.path] = Process.run(
+    fResults.add(TestProcess.run(
       'dart',
       [
         '--enable-experiment=non-nullable',
         file.path,
       ],
-    );
+    ));
   }
 
-  print('Running test ... ');
-  results.forEach((path, result) {
-    result.then((res) {
-      print('  dart --enable-experiment=non-nullable $path.');
-      print(indent(res.stdout, 4, colorOutput: colorOutput));
+  // Printing results.
+  fResults.forEach((fResult) {
+    fResult.then((result) {
+      print('Running test: ${result.command}');
+      print(result.formattedStdout(
+        indentMultiplier: 2,
+        colorOutput: colorOutput,
+      ));
       if (isVerbose) {
         // Indenting stderr output by 10 spaces.
-        print(indent(res.stderr, 10));
+        print(result.formattedStderr(
+          indentMultiplier: 10,
+          colorOutput: colorOutput,
+        ));
       }
-      exitCodes[path]?.complete(res.exitCode);
-      stderrOutput[path]?.complete(res.stderr);
     });
   });
 
-  //Future.wait(results).then((list) {});
-
-  await exitMessage(
-    exitCodes: exitCodes,
-    stderrOutput: stderrOutput,
+  // Composing exit message.
+  final results = await Future.wait(fResults);
+  final exitMsg = exitMessage(
+    results: results,
     colorOutput: colorOutput,
     isVerbose: isVerbose,
-  ).then<ExitMessage>((item) {
-    print(item.message);
-    exit(item.code);
-  });
+  );
+
+  // Exiting.
+  print(exitMsg.message);
+  exit(exitMsg.code);
 }
